f4bb7693e6b23a11f4b69dec30101f1b92368d78
GRASiN1 <gauravrajsingh047@gmail.com>
2025-12-16 23:21:33 +0530

    update cors

--- config/Cors.js
+++ config/Cors.js
@@ -2,10 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3001", // Local development
     "http://localhost:3000", // Local development
-    "https://paymatetrack.netlify.app", // Netlify frontend
-    "https://paymate-steel.vercel.app", // Vercel frontend
-    "https://paymate-steel.vercel.app",
-    "https://paymate-ruddy.vercel.app",
+    "https://paymate-frontend.vercel.app"
   ],
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: ["Content-Type", "Authorization"],
